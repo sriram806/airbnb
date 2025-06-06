c689a7116d247a5134f2beb112c5d3aa45f5b4c3
sriram806 <boddusriram1234@gmail.com>
2025-06-06 22:32:54 +0530

    Setting up project

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+
+const nunito = Nunito({
+  variable: "--font-nunito",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Airbnb",
+  description: "An Airbnb clone built with Next.js and React",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${nunito.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
