71709ff8791a4e21fd4c49e601dccaf8ce9e06c4
sriram806 <boddusriram1234@gmail.com>
2025-06-06 23:49:34 +0530

    Completing a full Navbar UI with MenuItem

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -27,7 +27,7 @@ function UserMenu() {
             </div>
             {isOpen && (
                 <div className="absolute right-0 top-12 w-[40vw] md:w-3/4 bg-white rounded-xl shadow-md overflow-hidden text-sm">
-                    <div className="flex flex-col cursor-pointer">
+                    <div className="flex flex-col  cursor-pointer">
                         <>
                         <MenuItem 
                             onClick={() => { }}
