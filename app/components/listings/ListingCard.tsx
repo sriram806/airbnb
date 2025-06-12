4fbdacf8df1f9fb7bea675347222f026d5861087
sriram806 <boddusriram1234@gmail.com>
2025-06-12 22:07:39 +0530

    Getting  all listing

---
+++ app/components/listings/ListingCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ListingCard() {
+  return (
+    <div>ListingCard</div>
+  )
+}
+
+export default ListingCard
--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default async function RootLayout({
           <LoginModel />
           <Navbar currentUser={currentUser}/>
         </ClientOnly>
-        {children}
+        <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
   );
